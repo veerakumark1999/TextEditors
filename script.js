78a89a801a71a602f7837271bf98f8e5b0270ad4
veerakumark1999 <129292982+veerakumark1999@users.noreply.github.com>
2023-07-03 20:18:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+import { Stack } from './stack.js';
+
+document.onkeydown = function(event) {
+    if (event.ctrlKey || event.metaKey) {
+        event.preventDefault();
+    }
+};
+
+onload = function () {
+    // Get reference to elements
+    const textbox = document.getElementById('comment');
+    const undo = document.getElementById('undo');
+    const clear = document.getElementById('clear');
+    const temptext = document.getElementById('temptext');
+
+    textbox.value = "";
+    let text = "";
+    let stack = new Stack();
+
+    textbox.onclick = function () {
+        textbox.selectionStart = textbox.selectionEnd = textbox.value.length;
+    };
+
+    clear.onclick = function () {
+        stack.clear();
+        text = "";
+        textbox.value = "";
+        temptext.innerHTML = `Navigate history will be shown here !`;
+    };
+
+    textbox.oninput = function(event){
+        
+        switch(event.inputType){
+            case "insertText":
+                stack.push(0, event.data);
+                break;
+            case "deleteContentBackward":
+                stack.push(1, text[text.length-1]);
+                break;
+        }
+
+        temptext.innerHTML = "On stack "+stack.top()+"<br>"+temptext.innerHTML;
+        text = textbox.value;
+    };
+
+    undo.onclick = function () {
+        let operation = stack.pop();
+        if(operation[0]!==-1){
+            temptext.innerHTML = "Performing undo operation<br>"+temptext.innerHTML;
+            if(operation[0] === 0){
+                let len = operation[1].length;
+                textbox.value = textbox.value.substring(0,textbox.value.length-len);
+            } else{
+                textbox.value += operation[1];
+            }
+            text = textbox.value;
+        }
+    };
+};
+const textarea = document.querySelector("textarea"),
+fileNameInput = document.querySelector(".file-name input"),
+selectMenu = document.querySelector(".save-as select"),
+saveBtn = document.querySelector(".save-btn");
+selectMenu.addEventListener("change", () => {
+    const selectedFormat = selectMenu.options[selectMenu.selectedIndex].text;
+    saveBtn.innerText = `Save As ${selectedFormat.split(" ")[0]} File`;
+});
+saveBtn.addEventListener("click", () => {
+    const blob = new Blob([textarea.value], {type: selectMenu.value});
+    const fileUrl = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.download = fileNameInput.value;
+    link.href = fileUrl;
+    link.click();
+});
